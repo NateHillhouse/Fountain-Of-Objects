50ffe77535e2343d57b51859eef663fa0eb48e9e
mizterwaffles <nhillhouse04@gmail.com>
2026-03-18 21:35:50 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+List<(int row, int column)> grid = [];
+for (int i= 1; i<=4; i++)
+{
+    for (int j = 1; j<=4; j++)
+    {
+        grid.Add((i, j));
+    }
+}
+foreach ((int row, int column) in grid)
+{
+    Console.WriteLine($"Row: {row}, Column: {column}");
+}
+
+
